702036432465284c20834b303060ae7189b8d148
Tommi Mäklin <tommi@maklin.fi>
2019-11-19 13:51:35 +0200

    use cxxargs to parse arguments

--- src/convert_alignments_unpaired.cpp
+++ src/convert_alignments_unpaired.cpp
@@ -6,6 +6,8 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include "cxxargs/include/cxxargs.hpp"
+
 struct ec_info {
   std::vector<bool> pseudoalignment;
   uint32_t count = 0;
@@ -146,31 +148,6 @@ std::unordered_map<std::vector<bool>, ec_info> CompressAlignments(const std::uno
   return compressed_ecs;
 }
 
-// void ReadFullAlignments2(const std::string &path, std::unordered_map<Pseudoalignment, uint32_t> &ecs) {
-//   Reader reader(path);
-//   if (reader.is_ready()) {
-//     while (reader.next_line()) {
-//       reader.partition_line();
-//       reader.next_part(' ');
-//       std::vector<bool> alignment(3815, false);
-//       uint32_t ec_id = std::stoi(reader.current_part());
-//       uint16_t cluster_id;
-//       bool any_aligned = false;
-//       while (reader.next_part(' ')) {
-// 	cluster_id = std::stoi(reader.current_part());
-// 	alignment[cluster_id] = true;
-// 	any_aligned = true;
-//       }
-//       if (any_aligned) {
-// 	Pseudoalignment pseudo(alignment);
-
-// 	ecs[alignment].count += 1;
-// 	ecs[alignment].last_val = std::max(cluster_id, ecs[alignment].last_val);
-//       }
-//     }
-//   }
-// }
-
 void WriteFullAlignments(const std::string &path, const std::unordered_map<std::vector<bool>, ec_info> &ecs) {
   std::ofstream ec_file(path + ".ec");
   std::ofstream tsv_file(path + ".tsv");
@@ -192,70 +169,30 @@ void WriteFullAlignments(const std::string &path, const std::unordered_map<std::
   }
 }
 
-// void ReadFullAlignments(const std::string &path, std::unordered_set<Pseudoalignment> &ecs) {
-//   std::ifstream file(path);
-//   if (file.is_open()) {
-//     std::string line;
-//     while (getline(file, line)) {
-//       std::vector<bool> alignment(3815, false);
-//       std::string part;
-//       std::stringstream partition(line);
-//       getline(partition, part, ' ');
-//       short unsigned ec_id = std::stoi(part);
-//       short unsigned cluster_id;
-//       bool any_aligned = false;
-//       while (getline(partition, part, ' ')) {
-// 	cluster_id = std::stoi(part);
-// 	alignment[cluster_id] = true;
-// 	any_aligned = true;
-//       }
-//       if (any_aligned) {
-// 	Pseudoalignment pseudo;
-// 	ecs.insert(pseudo);
-// 	//	ecs[alignment].alignment = alignment;
-// 	//	ecs[alignment].count += 1;
-// 	//	ecs[alignment].last_val = cluster_id;
-//       }
-//     }
-//   }
-// }
+void parse_args(int argc, char* argv[], cxxargs::Arguments &args) {
+  args.add_short_argument<std::string>('1', "Pseudoalignments for strand 1.");
+  args.add_short_argument<std::string>('2', "Pseudoalignments for strand 2.");
+  args.add_short_argument<std::string>('o', "Output files prefix.");
+  args.add_long_argument<uint32_t>("n-refs", "Number of reference sequences in the pseudoalignment.");
+  args.parse(argc, argv);
+}
 
-// void WriteFullAlignments(const std::string &path, const std::unordered_set<Pseudoalignment> &ecs) {
-//   std::ofstream ec_file(path + ".ec");
-//   std::ofstream tsv_file(path + ".tsv");
-//   if (ec_file.is_open() && tsv_file.is_open()) {
-//     short unsigned ec_id = 0;
-//     for (auto ec : ecs) {
-//       ec_file << ec_id << '\t';
-//       tsv_file << ec_id << '\t';
-//       tsv_file << ec.count << '\n';
-//       for (size_t i = 0; i < ec.alignment.size(); ++i) {
-// 	if (ec.alignment.at(i)) {
-// 	  ec_file << i << (i == ec.last_val ? '\n' : ',');
-// 	}
-//       }
-//       ++ec_id;
-//     }
-//     ec_file.close();
-//     tsv_file.close();
-//   }
-// }
- 
 int main(int argc, char* argv[]) {
-  std::string infile_1 = argv[1];
-  std::string infile_2 = argv[2];
-  std::string outfile = argv[3];
-  uint32_t n_refs = std::stoi(argv[4]);
+  cxxargs::Arguments args("telescope-unpaired", "Usage:...");
+  try {
+    std::cerr << "Parsing arguments" << '\n';
+    parse_args(argc, argv, args);
+  } catch (std::exception &e) {
+    std::cerr << "Parsing arguments failed:"
+	      << '\t' << e.what()
+	      << "\nexiting" << std::endl;
+    return 0;
+  }
   std::unordered_map<std::vector<bool>, ec_info> ecs;
-  // std::unordered_map<uint32_t, ec_info> ecs;
-  // std::unordered_set<Pseudoalignment, PseudoalignmentHash, PseudoalignmentEqual_to> ec_pseudos;
 
-  ReadFullAlignments(infile_1, ecs, n_refs);
-  ReadFullAlignments(infile_2, ecs, n_refs);
-  // ReadIntersectionAlignments(infile_1, ecs, n_refs);
-  // ReadIntersectionAlignments(infile_2, ecs, n_refs);
-  // const std::unordered_map<std::vector<bool>, ec_info> &cmp_ecs = CompressAlignments(ecs);
-  WriteFullAlignments(outfile, ecs);
-  std::cout << "Finished, exiting..." << std::endl;
+  ReadFullAlignments(args.value<std::string>('1'), ecs, args.value<uint32_t>("n-refs"));
+  ReadFullAlignments(args.value<std::string>('2'), ecs, args.value<uint32_t>("n-refs"));
+  WriteFullAlignments(args.value<std::string>('o'), ecs);
+  std::cerr << "Finished, exiting..." << std::endl;
   return 0;
 }
