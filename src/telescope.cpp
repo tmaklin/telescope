3889b4041d8b8b461cfd6c995d2963a37ce2409f
Tommi Mäklin <tommi@maklin.fi>
2019-12-12 11:51:13 +0200

    add option to print help/usage info

--- src/telescope.cpp
+++ src/telescope.cpp
@@ -16,12 +16,21 @@ namespace cxxargs {
 #include "cxxargs/include/cxxargs.hpp"
 #include "file.hpp"
 
-void parse_args(int argc, char* argv[], cxxargs::Arguments &args) {
+bool CmdOptionPresent(char **begin, char **end, const std::string &option) {
+  return (std::find(begin, end, option) != end);
+}
+
+void parse_args(int argc, char* argv[], cxxargs::Arguments &args, File::Out &log) {
   args.add_short_argument<std::string>('1', "Themisto pseudoalignments for strand 1.");
   args.add_short_argument<std::string>('2', "Themisto pseudoalignments for strand 2.");
   args.add_short_argument<std::string>('o', "Output files prefix.");
   args.add_long_argument<uint32_t>("n-refs", "Number of reference sequences in the pseudoalignment.");
   args.add_long_argument<Mode>("mode", "How to merge paired-end alignments (one of unpaired, union, intersection; default: unpaired)", m_unpaired);
+  args.add_long_argument<bool>("help", "Print the help message.", false);
+  if (CmdOptionPresent(argv, argv+argc, "--help")) {
+    log << args.help() << '\n' << '\n';
+    log.flush();
+  }
   args.parse(argc, argv);
 }
 
@@ -30,10 +39,10 @@ int main(int argc, char* argv[]) {
   cxxargs::Arguments args("telescope", "Usage: telescope -1 <strand_1> -2 <strand_2> -o <output prefix> --n-refs <number of references>");
   try {
     log << "Parsing arguments" << '\n';
-    parse_args(argc, argv, args);
+    parse_args(argc, argv, args, log);
   } catch (std::exception &e) {
     log << "Parsing arguments failed:"
-	<< '\t' << e.what()
+	<< ' ' << e.what()
 	<< "\nexiting" << '\n';
     log.flush();
     return 1;
