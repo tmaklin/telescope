afe3ed6a60c5d222e21fa62df4892a9cd800d5b9
Tommi Mäklin <tommi@maklin.fi>
2019-12-12 16:23:13 +0200

    add rudimentary logging and the option to disable it

---
+++ include/log.hpp
@@ -0,0 +1,48 @@
+#ifndef TELESCOPE_LOG_HPP
+#define TELESCOPE_LOG_HPP
+
+#include <chrono>
+#include <fstream>
+#include <exception>
+#include <string>
+
+#include "file.hpp"
+
+class Log : public File::Out {
+ private:
+  std::chrono::time_point<std::chrono::system_clock> start_time;
+
+ public:
+  bool verbose;
+
+  Log(std::ostream &stream, bool verbose = true) : File::Out(stream), verbose(verbose) {
+    start_time = std::chrono::system_clock::now();
+  }
+  void flush() {
+    if (verbose) {
+      std::chrono::time_point<std::chrono::system_clock> end_time = std::chrono::system_clock::now();
+      std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+      std::time_t end = std::chrono::system_clock::to_time_t(end_time);
+      std::string end_s(std::ctime(&end));
+      end_s.pop_back();
+      *this << end_s << " elapsed_time: " << elapsed_seconds.count() << "s\n";
+    }
+  }
+};
+
+template <typename T>
+Log& operator<<(Log &os, T t) {
+  if (os.verbose) {
+    std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+    std::time_t now_t = std::chrono::system_clock::to_time_t(now);
+    std::string now_s(std::ctime(&now_t));
+    now_s.pop_back();
+    os.stream() << now_s << ' ' << t;
+    if (!os.stream().good() && !os.stream().eof()) {
+      throw std::runtime_error("Error writing type: " + std::string(typeid(T).name()) + " to file " + os.filename());
+    }
+  }
+  return os;
+}
+
+#endif
--- src/telescope.cpp
+++ src/telescope.cpp
@@ -5,6 +5,7 @@
 #include "cxxargs/include/cxxargs.hpp"
 
 #include "file.hpp"
+#include "log.hpp"
 
 bool CmdOptionPresent(char **begin, char **end, const std::string &option) {
   return (std::find(begin, end, option) != end);
@@ -15,28 +16,31 @@ void parse_args(int argc, char* argv[], cxxargs::Arguments &args, File::Out &log
   args.add_short_argument<std::string>('o', "Output file directory.");
   args.add_long_argument<uint32_t>("n-refs", "Number of reference sequences in the pseudoalignment.");
   args.add_long_argument<Mode>("mode", "How to merge paired-end alignments (one of unpaired, union, intersection; default: unpaired)", m_unpaired);
+  args.add_long_argument<bool>("silent", "Suppress status messages (default: false)", false);
   args.add_long_argument<bool>("help", "Print the help message.", false);
   if (CmdOptionPresent(argv, argv+argc, "--help")) {
-    log << args.help() << '\n' << '\n';
+    log << "\n" + args.help() << '\n' << '\n';
     log.flush();
   }
   args.parse(argc, argv);
 }
 
 int main(int argc, char* argv[]) {
-  File::Out log(std::cerr);
+  Log log(std::cerr, !CmdOptionPresent(argv, argv+argc, "--silent"));
   cxxargs::Arguments args("telescope", "Usage: telescope -r <strand_1>,<strand_2> -o <output prefix> --mode <merge mode> --n-refs <number of references>");
+  log << args.get_program_name() + '\n';
   try {
-    log << "Parsing arguments" << '\n';
+    log << "Parsing arguments\n";
     parse_args(argc, argv, args, log);
   } catch (std::exception &e) {
-    log << "Parsing arguments failed:"
-	<< ' ' << e.what()
-	<< "\nexiting" << '\n';
+    log.verbose = true;
+    log << "Parsing arguments failed:\n"
+	<< std::string("\t") + std::string(e.what()) + "\n"
+	<< "\trun telescope with the --help option for usage instructions.\n";
     log.flush();
     return 1;
   }
-  log << "Reading Themisto alignments" << '\n';
+  log << "Reading Themisto alignments\n";
   std::vector<File::In> infiles(args.value<std::vector<std::string>>('r').size());
   std::vector<std::istream*> infile_ptrs(infiles.size());
   for (size_t i = 0; i < args.value<std::vector<std::string>>('r').size(); ++i) {
@@ -50,19 +54,19 @@ int main(int argc, char* argv[]) {
     alignments.call += (i == argc - 1 ? "" : " ");
   }
 
-  log << "Writing converted alignments" << '\n';
+  log << "Writing converted alignments\n";
   File::Out ec_file(args.value<std::string>('o') + "/pseudoalignments.ec");
   File::Out tsv_file(args.value<std::string>('o') + "/pseudoalignments.tsv");
   WriteAlignments(alignments.ecs, &ec_file.stream(), &tsv_file.stream());
 
-  log << "Writing read assignments to equivalence classes" << '\n';
+  log << "Writing read assignments to equivalence classes\n";
   File::Out read_to_ref_file(args.value<std::string>('o') + "/read-to-ref.txt");
   WriteReadToRef(alignments.read_to_ref, &read_to_ref_file.stream());
 
   File::Out run_info_file(args.value<std::string>('o') + "/run_info.json");
   WriteRunInfo(alignments, &run_info_file.stream());
 
-  log << "Done" << '\n';
+  log << "Done\n";
   log.flush();
 
   return 0;
