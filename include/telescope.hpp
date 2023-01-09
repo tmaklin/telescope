d3eda20af835d6801a6e689100d5b0d30d496c12
Tommi Mäklin <tommi@maklin.fi>
2023-01-09 16:14:16 +0200

    Add documentation.

--- include/telescope.hpp
+++ include/telescope.hpp
@@ -31,31 +31,62 @@
 #include "KallistoAlignment.hpp"
 
 namespace telescope {
+namespace read {
+  // Check `read_themisto_alignments.hpp`
+}
+
+namespace write {
+// Functions for writing the telescope::Alignment objects
+
+// telescope::write::ThemistoToKallisto
+//
+// Writes the alignment contained in `aln` into files matching the Kallisto format.
+//
+// Input:
+//   `aln`: The pseudoalignment to write.
+//   `ec_file`: Pointer to the file that will store the equivalence class configurations.
+//   `tsv_file`: Pointer to the file that will contain the observation counts of each equivalence class.
+void ThemistoToKallisto(const ThemistoAlignment &aln, std::ostream* ec_file, std::ostream* tsv_file);
+
+// telescope::write::ThemistoReadAssignments
+//
+// Writes the alignment of each read against the reference sequences.
+//
+// Input:
+//   `aln`: The pseudoalignment to write.
+//   `out`: Pointer to the output file stream.
+void ThemistoReadAssignments(const ThemistoAlignment &aln, std::ostream* out);
+
+// telescope::write::KallistoInfoFile
+//
+// Writes the Kallisto run_info.json file from the `run_info` object.
+//
+// Input:
+//   `run_info`: The KallistoRunInfo object to write.
+//   `indent_len`: Indent length in the written json file.
+//   `out`: Pointer to a file stream to write into.
+void KallistoInfoFile(const KallistoRunInfo &run_info, const uint8_t indent_len, std::ostream *out);
+}
+
+// telescope::get_mode returns the correct set operation for merging
+// paired alignments. The return value is used as an argument to the
+// telescope::read functions.
 inline bm::set_operation get_mode(const std::string &mode_str) {
   // Get the paired reads merge mode based on command line argument
   if (mode_str == "union") return bm::set_OR;
   if (mode_str == "intersection") return bm::set_AND;
   throw std::runtime_error("Unrecognized paired-end mode.");
 }
-
-namespace read {
-  // Check `read_themisto_alignments.hpp`
-}
-
-namespace write {
-void ThemistoToKallisto(const ThemistoAlignment &aln, std::ostream* ec_file, std::ostream* tsv_file);
-void ThemistoReadAssignments(const ThemistoAlignment &aln, std::ostream* out);
-void KallistoInfoFile(const KallistoRunInfo &run_info, const uint8_t indent_len, std::ostream *out);
-}
 }
 
 namespace cxxargs {
-  inline std::istream& operator>> (std::istream &in, bm::set_operation &t) {
-    std::string in_val;
-    in >> in_val;
-    t = telescope::get_mode(in_val);
-    return in;
-  }
+// Define the cxxargs operator for reading a command-line argument as a bm::set_operation.
+inline std::istream& operator>> (std::istream &in, bm::set_operation &t) {
+  std::string in_val;
+  in >> in_val;
+  t = telescope::get_mode(in_val);
+  return in;
+}
 }
 
 #endif
